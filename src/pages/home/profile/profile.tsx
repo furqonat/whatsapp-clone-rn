b936a6770cd9636aea44b9b56f13408348cdc3cb
hendrawandadan <dadanhendrawan55@gmail.com>
2022-11-20 20:26:15 +0700

    add profile and qr

---
+++ src/pages/home/profile/profile.tsx
@@ -0,0 +1,29 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Button, Image, Input, Stack } from 'native-base'
+
+const Profile = () => {
+  return (
+    <Stack top={8} h={'100%'} >
+        <Stack alignItems={'center'}>
+            <Image 
+             size={'xl'}
+             borderRadius={100}
+             borderColor={'black'}
+             src={`https://avatars.dicebear.com/api/avataaars/${Date.now()}.png`}
+             alt={" "}
+             mb={'8'}
+             />
+        </Stack>
+        <Stack   space={4} w="75%" maxW="300px" mx="auto">
+            <Input fontSize={'md'}   variant="underlined" placeholder='Nama'/>
+            <Input fontSize={'md'}  variant="underlined" placeholder='Email'/>
+            <Input fontSize={'md'}  variant="underlined" placeholder='No KTP' keyboardType='numeric'/>
+            <Input fontSize={'md'}  variant="underlined" placeholder='Alamat'/>
+            <Button>Simpan</Button>
+        </Stack>
+    </Stack>
+  )
+}
+
+export {Profile}
